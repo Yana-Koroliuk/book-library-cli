1fbd118034b4cfc0331f1ba35221f333f403de6e
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2023-03-27 21:49:54 +0300

    Add sql files

---
+++ src/db/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO Author(id, name) VALUES (1, 'Aurelius');
+INSERT INTO Author(id, name) VALUES (2, 'Mark Twain');
+
+INSERT INTO Category(id, name) VALUES (1, 'philosophy');
+INSERT INTO Category(id, name) VALUES (2, 'adventure');
+
+INSERT INTO Book(id, title, category_id) VALUES (1, 'Meditation', 1);
+INSERT INTO Book(id, title, category_id) VALUES (2, 'Tom Sawyer', 2);
+INSERT INTO Book(id, title, category_id) VALUES (3, 'Bible', 1);
+INSERT INTO Book(id, title, category_id) VALUES (4, 'Quran', 1);
+
+
+INSERT INTO Book_Author(book_id, author_id) VALUES (1, 1);
+INSERT INTO Book_Author(book_id, author_id) VALUES (3, 1);
+
+DELETE FROM Author WHERE id=1;
+SELECT * FROM Author;
---
+++ src/db/structure.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Author (
+    id bigint PRIMARY KEY,
+    name varchar(255) NOT NULL
+);
+
+CREATE TABLE Category (
+    id int PRIMARY KEY,
+    name varchar(255) NOT NULL
+);
+
+CREATE TABLE Book (
+    id bigint PRIMARY KEY,
+    title varchar(255) NOT NULL,
+    category_id int NOT NULL,
+    CONSTRAINT fk_category_id
+        FOREIGN KEY (category_id)
+            REFERENCES Category(id)
+                  ON DELETE CASCADE
+);
+
+CREATE TABLE Book_Author (
+    book_id bigint NOT NULL,
+    author_id bigint NOT NULL,
+    PRIMARY KEY (book_id, author_id),
+    CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES Book(id) ON DELETE CASCADE,
+    CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES Author(id) ON DELETE CASCADE
+);
