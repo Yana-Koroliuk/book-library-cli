5dbb875171e2b53e466e60949805aa1697a30bb2
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2023-05-09 00:20:02 +0300

    Update sql files

--- src/db/data.sql
+++ src/db/data.sql
@@ -10,8 +10,7 @@ INSERT INTO Book(id, title, category_id) VALUES (3, 'Bible', 1);
 INSERT INTO Book(id, title, category_id) VALUES (4, 'Quran', 1);
 
 
-INSERT INTO Book_Author(book_id, author_id) VALUES (1, 1);
-INSERT INTO Book_Author(book_id, author_id) VALUES (3, 1);
-
-DELETE FROM Author WHERE id=1;
-SELECT * FROM Author;
+INSERT INTO Book_Author (book_id, author_id) VALUES (1, 1);
+INSERT INTO Book_Author (book_id, author_id) VALUES (2, 2);
+INSERT INTO Book_Author (book_id, author_id) VALUES (3, 1);
+INSERT INTO Book_Author (book_id, author_id) VALUES (4, 1);
--- src/db/structure.sql
+++ src/db/structure.sql
@@ -25,3 +25,20 @@ CREATE TABLE Book_Author (
     CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES Book(id) ON DELETE CASCADE,
     CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES Author(id) ON DELETE CASCADE
 );
+
+CREATE TABLE User {
+    id bigint PRIMARY KEY,
+    name varchar(255) NOT NULL
+};
+
+CREATE TABLE Checkout {
+    id bigint PRIMARY KEY,
+    start_time DATA, NOT NULL
+    end_time DATA, NOT NULL
+    user_id int NOT NULL,
+    CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id)
+            REFERENCES User(id)
+                ON DELETE CASCADE
+    isReturned BOOLEAN
+};
