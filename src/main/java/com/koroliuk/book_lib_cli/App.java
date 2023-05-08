fb250ec2d2198bcf1a7e2aaf1a536b83bbf45b49
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2023-05-09 00:23:14 +0300

    Update App.java

--- src/main/java/com/koroliuk/book_lib_cli/App.java
+++ src/main/java/com/koroliuk/book_lib_cli/App.java
@@ -6,13 +6,8 @@ public class App {
     public static void main(String[] args) throws SQLException {
         DbManager dbManager = DbManager.getInstance();
         Connection connection = dbManager.getConnection();
-        String sql = "INSERT INTO Author(id, name) VALUES (1, 'Aurelius')";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.executeUpdate();
-        String sql1 = "UPDATE Author set NAME = 'Aram' where ID = 1";
-        PreparedStatement preparedStatement1 = connection.prepareStatement(sql1);
-        preparedStatement1.executeUpdate();
-        preparedStatement.close();
-        preparedStatement1.close();
+        QueryManager queryManager = new QueryManager();
+        Controller controller = new Controller(queryManager);
+        controller.run();
     }
 }
